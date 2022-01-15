f5ccfcb0881b9b098585ef4d8af964c3ee458bac
Mathieu KHALEM <mathieu.khalem@gmail.com>
2022-01-15 17:19:38 +0100

    Add a board builder from string

--- src/main/kotlin/fr/khalypso/wordblitz/board/BoardFactory.kt
+++ src/main/kotlin/fr/khalypso/wordblitz/board/BoardFactory.kt
@@ -6,12 +6,22 @@ class BoardFactory {
         fun fromConsoleInput(): Board {
             val cases = readLine()!!.trim().toCharArray().map { Case(it) }
             val gameMatrix = listOf(
-                cases.subList(0,4),
+                cases.subList(0, 4),
                 cases.subList(4, 8),
                 cases.subList(8, 12),
                 cases.subList(12, 16)
-            );
-            return Board(gameMatrix);
+            )
+            return Board(gameMatrix)
+        }
+        fun fromString(letters: String): Board {
+            val cases = letters.toCharArray().map { Case(it) }
+            val gameMatrix = listOf(
+                cases.subList(0, 4),
+                cases.subList(4, 8),
+                cases.subList(8, 12),
+                cases.subList(12, 16)
+            )
+            return Board(gameMatrix)
         }
     }
 }
